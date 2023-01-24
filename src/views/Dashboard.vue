21ae626a540f17f8d67b16554927d3527c785b5f
oliviagb97 <ogbasanta@gmail.com>
2023-01-24 04:49:06 -0500

    commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default{
+    name: "Dashboard"
+}
+</script>
+
+<template>
+<div>
+    Este es el dashboard
+</div>
+</template>
+
+<style lang="stylus" scoped>
+</style>
